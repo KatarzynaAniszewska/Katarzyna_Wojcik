040a122a1404f59177a976cdd0484c3639e3cdf3
Katarzyna <katarzyna.aniszewska@gmail.com>
2017-12-07 15:45:16 +0100

    Second Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,12 +1,18 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main(String []args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowani");
+
+import com.kodilla.testing.user.SimpleUser;
+
+    public class TestingMain {
+        public static void main(String[] args){
+            SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+            String result = simpleUser.getUsername();
+
+            if (result.equals("theForumUser")){
+                System.out.println("test OK");
+            } else {
+                System.out.println("Error!");
+            }
+        }
     }
-
-
-
-
-
-}
