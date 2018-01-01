b356d0f93b953b4d3936ce89c1dbc8c9d6acb6d9
Katarzyna <katarzyna.aniszewska@gmail.com>
2018-01-01 09:08:22 +0100

    9.4

---
+++ kodilla-good-patterns/src/main/java/FlightSearch/Airport.java
@@ -0,0 +1,96 @@
+package FlightSearch;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.lang.Integer.parseInt;
+
+public class Airport {
+    String cityName;
+    Set<Airport> arrivalAirports = new HashSet<>();
+    Set<Set<Airport>> departureAirports = new HashSet<>();
+
+
+    public Set<Set<Airport>> getDepartureAirports() {
+        return departureAirports;
+
+    }
+    public Airport(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public Set<Airport> getArrivalAirports() {
+        return arrivalAirports;
+    }
+
+    public void addArrivalAirport(Airport airport) {
+         arrivalAirports.add(airport);
+
+    }
+    public void addDepartureAirport(Set<Airport>arrivalAirports){
+
+        departureAirports.add(arrivalAirports);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Airport airport = (Airport) o;
+
+        if (!cityName.equals(airport.cityName)) return false;
+        return arrivalAirports.equals(airport.arrivalAirports);
+    }
+
+    @Override
+    public int hashCode() {
+        return cityName.hashCode();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "cityName='" + cityName + '\'' +
+                '}';
+    }
+
+    public Set<String> getArrivalCityDirectFlight(String cityName) {
+        return arrivalAirports.stream()
+                .map(Airport::getCityName)
+                .collect(Collectors.toSet());
+
+
+    }
+
+
+    public Set<String> getArrivalCityTransferFlight(String cityName){
+        return arrivalAirports.stream()
+                .flatMap(airport -> airport.getArrivalAirports().stream())
+                .filter(airport -> airport !=this)
+                .map(Airport::getCityName)
+                .collect(Collectors.toSet());
+
+    }
+    //To nie jest właściwe rozwiązanie, ale nie ma pomysłu jak wyciągnąć właściwe dane
+    public Set<String > getDeparture(String cityName){
+    return departureAirports.stream()
+            .flatMap(d->d.stream())
+            .map(x->x.getCityName())
+            .filter(x1->x1.equals(cityName))
+            .collect(Collectors.toSet());
+
+
+
+
+    }
+
+
+}
