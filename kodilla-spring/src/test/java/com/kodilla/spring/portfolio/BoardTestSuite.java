724db332434f400ea117f712d33589b70e529b72
Katarzyna <katarzyna.aniszewska@gmail.com>
2018-01-06 14:02:46 +0100

    "spring"

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardTestSuite {
+
+    @Test
+    public void testAddTask(){
+
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(BoardConfig.class);
+
+        //When
+        TaskList taskList1 = (TaskList)context.getBean("TaskList1");
+        TaskList taskList2 = (TaskList)context.getBean("TaskList2");
+        TaskList taskList3 = (TaskList)context.getBean("TaskList3");
+        boolean taskListExist = context.containsBean("TaskList1");
+        Board board1 = (Board) context.getBean("getBoard");
+        boolean board1Exists = context.containsBean("getBoard");
+
+
+        board1.getToDoLIst().getTasks().add("===Example To do===");
+        board1.getInProgressList().getTasks().add("===Example In prpogress===");
+        board1.getToDoLIst().getTasks().add("===Example done===");
+
+
+
+        //Then
+        System.out.println(board1);
+
+    }
+}
